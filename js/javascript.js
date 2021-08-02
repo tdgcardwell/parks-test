62ff9597015d64774f65e0d4d73804df87dc5fa6
Thomas Cardwell <Tom@ThomasCardwell.com>
2021-08-01 20:23:54 -0400

    drop-in of files

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+// lets see if this can work...
+
+
+// borrowed random generating code
+// https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+
+const parks = ["Skiles Test",
+                "Heritage",
+                "Founders",
+                "River Road",
+                "School and Goats",
+                "Sahm",
+                "Central"];
+
+const random = Math.floor(Math.random() * parks.length);
+console.log(random, parks[random]);
+
+
+
+// borrowed text code
+var paragraph = document.getElementById("p");
+var text = document.createTextNode(parks[random]);
+// https://stackoverflow.com/questions/41764061/adding-text-to-an-existing-text-element-in-javascript-via-dom
+
+function whichPark() {
+  paragraph.appendChild(text);
+}
+
+function refreshPage() {
+  location.reload();
+}
---
+++ scratch.md
@@ -0,0 +1,13 @@
+# Parks Test
+
+## 2nd Heading
+
+Test Paragraph
+
+## Third Heading
+
+Second Paragraph
+
+## Another Heading
+
+Another Paragraph
